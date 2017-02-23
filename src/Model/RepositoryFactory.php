d811b6d6192c41e3d0d4b4f296fccbbd9b5d0eb7
Luiz Smelán <luizcarlosj@gmail.com>
2017-02-23 19:40:56 +0000

    Added configuration yaml file

--- src/Model/RepositoryFactory.php
+++ src/Model/RepositoryFactory.php
@@ -2,6 +2,8 @@
 
 namespace Model;
 
+use Symfony\Component\Yaml\Yaml;
+
 class RepositoryFactory
 {
     /**
@@ -12,11 +14,17 @@ class RepositoryFactory
      */
     public function getInstance($name, $driver = 'mysql', $type = 'SQL')
     {
+        $config = Yaml::parse(file_get_contents('../config.yml'));
         $className = 'Infrastructure\\Persistence\\' . $type . '\\' . ucfirst($name) . 'Repository';
 
         switch (strtoupper($type)) {
             case 'SQL':
-                $pdo = new \PDO($driver . ':host=localhost;dbname=challenge', 'root', 'root');
+                $host = $config['databases'][$driver]['host'];
+                $dbname = $config['databases'][$driver]['dbname'];
+                $user = $config['databases'][$driver]['user'];
+                $password = $config['databases'][$driver]['password'];
+
+                $pdo = new \PDO("$driver:host=$host;dbname=$dbname", $user, $password);
                 $repository = new $className($pdo);
                 break;
             default:
